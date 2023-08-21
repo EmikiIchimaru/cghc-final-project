f0e1e7b703fb16a27004a44549d4402ab0808286
Emiki <benheng02624@gmail.com>
2023-08-22 07:25:42 +0800

    working on stamina and wallclimb

--- CGHC Final Project/Assets/Scripts/Ben Stuff/PlayerJumpBen.cs
+++ CGHC Final Project/Assets/Scripts/Ben Stuff/PlayerJumpBen.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerJump : PlayerStates
+public class PlayerJumpBen : PlayerStates
 {
     [Header("Settings")]
     [SerializeField] private float jumpHeight = 5f;
-    [SerializeField] private int maxJumps = 2;
+    [SerializeField] private int maxJumps = 1;
 
     private int _jumpAnimatorParameter = Animator.StringToHash("Jump");
     private int _doubleJumpParameter = Animator.StringToHash("DoubleJump");
@@ -15,18 +15,23 @@ public class PlayerJump : PlayerStates
     // Return how many jumps we have left
     public int JumpsLeft { get; set; }
 
+    private PlayerWallClimb wallComp;
+    private bool hasWallComp {get { return wallComp;} }
+
     protected override void InitState()
     {
         base.InitState();
         JumpsLeft = maxJumps;
+        wallComp = GetComponent<PlayerWallClimb>();
     }
 
     public override void ExecuteState()
     {
-        if (_playerController.Conditions.IsCollidingBelow && _playerController.Force.y == 0f)
+        if (_playerController.Conditions.IsCollidingBelow ) //&& _playerController.Force.y == 0f)
         {
             JumpsLeft = maxJumps;
             _playerController.Conditions.IsJumping = false;
+            if (hasWallComp) { wallComp.ResetStamina(); } 
         }
     }
 
@@ -34,42 +39,28 @@ public class PlayerJump : PlayerStates
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Jump();
+            if (_playerController.Conditions.IsWallClinging)
+            {
+                Jump();
+                if (hasWallComp) { wallComp.stamina -= 0.5f; }   
+            }
+
+            if (JumpsLeft > 0) 
+            {
+                JumpsLeft -= 1;
+                Jump();
+            }
+            
+            
         }
     }
 
     private void Jump()
     {
-        if (!CanJump())
-        {
-            return;
-        }
-
-        if (JumpsLeft == 0)
-        {
-            return;
-        }
-
-        JumpsLeft -= 1;
         
         float jumpForce = Mathf.Sqrt(jumpHeight * 2f * Mathf.Abs(_playerController.Gravity));
         _playerController.SetVerticalForce(jumpForce);
         _playerController.Conditions.IsJumping = true;
-    }
-
-    private bool CanJump()
-    {
-        if (!_playerController.Conditions.IsCollidingBelow && JumpsLeft <= 0)
-        {
-            return false;
-        }
-
-        if (_playerController.Conditions.IsCollidingBelow && JumpsLeft <= 0)
-        {
-            return false;
-        }
-
-        return true;
     }
 
     public override void SetAnimation()
---
+++ CGHC Final Project/Assets/Scripts/Ben Stuff/PlayerWallClimb.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWallClimb : PlayerStates
+{
+    [Header("Settings")] 
+    [SerializeField] private float fallFactor = 0.5f;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float maxStamina = 1.2f;
+    public float stamina;
+
+    private float _movement;
+    private bool canClimb { get { return stamina > 0f; } }
+    //private float _verticalMovement;
+    
+    protected override void InitState()
+    {
+        base.InitState();
+        stamina = maxStamina;
+    }
+
+    protected override void GetInput()
+    {
+        if (_horizontalInput <= -0.1f || _horizontalInput >= 0.1f)
+        {   
+            if (!canClimb) { return; }
+            WallCling();
+        }
+    }
+
+    public override void ExecuteState()
+    {
+        if (_playerController.Conditions.IsWallClinging)
+        {
+            CheckExitWallCling();
+            WallClimb();
+        }
+        
+    }
+
+    private void WallCling()
+    {
+        if (_playerController.Conditions.IsCollidingBelow || _playerController.Force.y >= 0) //on the FLOOR or in the AIR
+        {
+            return;
+        }
+
+        if (_playerController.Conditions.IsCollidingLeft && _horizontalInput <= -0.1f ||
+            _playerController.Conditions.IsCollidingRight && _horizontalInput >= 0.1f)
+        {
+            _playerController.SetWallClingMultiplier(fallFactor);
+            _playerController.Conditions.IsWallClinging = true;
+        }
+    }
+
+    private void CheckExitWallCling()
+    {
+
+        if (_playerController.Conditions.IsCollidingBelow || _playerController.Force.y >= 0)
+        {
+            ExitWallCling();
+            return;
+        }
+
+        if (_playerController.MovePosition.x > 0.01f || _playerController.MovePosition.x < -0.01f )
+        {
+            ExitWallCling();
+            return;
+        }
+
+        if (_playerController.FacingRight && _horizontalInput < 0.01f)
+        {
+            ExitWallCling();  
+            return;
+        }
+
+        if (!_playerController.FacingRight && _horizontalInput > -0.01f)
+        {
+            ExitWallCling();
+            return;
+        }
+    }
+
+    private void ExitWallCling()
+    {
+        _playerController.SetWallClingMultiplier(0f);
+        _playerController.Conditions.IsWallClinging = false;
+    }
+
+    private void WallClimb()
+    {
+        if (Mathf.Abs(_verticalInput) < 0.1f) { return; }
+
+        if (!canClimb) { return; }
+
+        _playerController.SetVerticalForce(_verticalInput * speed);
+
+        StartCoroutine(BurnStamina());
+    }
+    
+    private IEnumerator BurnStamina()
+    {
+        if (stamina > 0f)
+        {
+            stamina -= Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public void ResetStamina()
+    {
+        stamina = maxStamina;
+    }
+    
+    /* private void ExitWallCling()
+    {
+        if (_playerController.Conditions.IsWallClinging)
+        {
+            if (_playerController.Conditions.IsCollidingBelow || _playerController.Force.y >= 0)
+            {
+                _playerController.SetWallClingMultiplier(0f);
+                _playerController.Conditions.IsWallClinging = false;
+            }
+
+            if (_playerController.FacingRight)
+            {
+                if (_horizontalInput <= -0.1f || _horizontalInput < 0.1f)
+                {
+                    _playerController.SetWallClingMultiplier(0f);
+                    _playerController.Conditions.IsWallClinging = false;
+                }
+            }
+            else
+            {
+                if (_horizontalInput >= 0.1f || _horizontalInput > -0.1f)
+                {
+                    _playerController.SetWallClingMultiplier(0f);
+                    _playerController.Conditions.IsWallClinging = false;
+                }
+            }
+        }
+    } */
+}   

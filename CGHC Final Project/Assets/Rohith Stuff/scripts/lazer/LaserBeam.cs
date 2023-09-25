d75338d0ef16c9a577a5f2ccb4b6bf74ffd83084
Rohii19 <142196054+Rohii19@users.noreply.github.com>
2023-09-25 08:40:41 +0800

    Update
    Working on laser collision

--- CGHC Final Project/Assets/Rohith Stuff/scripts/lazer/LaserBeam.cs
+++ CGHC Final Project/Assets/Rohith Stuff/scripts/lazer/LaserBeam.cs
@@ -1,44 +1,62 @@
 using UnityEngine;
 using System.Collections;
+using System;
+using System.Collections.Generic;
+
 
 public class AnimatedLaserBeam : MonoBehaviour
 {
+
     public Transform laserEnd; // Reference to the end point of the laser beam
     public float duration = 2.0f; // Duration of the laser animation in seconds
     public float pauseDuration = 1.0f; // Duration to pause the laser beam in seconds
 
     private LineRenderer lineRenderer;
     private Vector3 startPosition;
-
-    void Start()
+    private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
         startPosition = transform.position;
         StartCoroutine(AnimateLaser());
+
+        IEnumerator AnimateLaser()
+        {
+            while (true)
+            {
+                // Animate the laser beam
+                float timer = 0.0f;
+                while (timer < duration)
+                {
+                    timer += Time.deltaTime;
+                    float t = Mathf.Clamp01(timer / duration);
+                    Vector3 newPosition = Vector3.Lerp(startPosition, laserEnd.position, t);
+                    lineRenderer.SetPosition(1, newPosition);
+                    yield return null;
+                }
+
+                // Pause the laser beam
+                yield return new WaitForSeconds(pauseDuration);
+
+                // Reset the LineRenderer's position
+                lineRenderer.SetPosition(1, startPosition);
+
+                // Optionally, you can add more logic here or break out of the loop to stop the sequence.
+            }
+        }
     }
 
-    IEnumerator AnimateLaser()
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        while (true)
+        // Check if the colliding GameObject has the "Player" tag
+        if (collision.gameObject.CompareTag("Player"))
         {
-            // Animate the laser beam
-            float timer = 0.0f;
-            while (timer < duration)
-            {
-                timer += Time.deltaTime;
-                float t = Mathf.Clamp01(timer / duration);
-                Vector3 newPosition = Vector3.Lerp(startPosition, laserEnd.position, t);
-                lineRenderer.SetPosition(1, newPosition);
-                yield return null;
-            }
-
-            // Pause the laser beam
-            yield return new WaitForSeconds(pauseDuration);
-
-            // Reset the LineRenderer's position
-            lineRenderer.SetPosition(1, startPosition);
-
-            // Optionally, you can add more logic here or break out of the loop to stop the sequence.
+            // Write a debug message to the console
+            Debug.Log("Colliding");
         }
     }
 }
+
+    
+

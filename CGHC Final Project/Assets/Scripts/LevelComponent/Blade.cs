b416736545ecd0af0d6e61ccdc3e509452e5d1b1
Rohii19 <142196054+Rohii19@users.noreply.github.com>
2023-11-06 14:53:21 +0800

    health reset after damage from enemy if killed by spike,(scene completed)

--- CGHC Final Project/Assets/Scripts/LevelComponent/Blade.cs
+++ CGHC Final Project/Assets/Scripts/LevelComponent/Blade.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 
 public class Blade : LevelComponent
 {
+    public int damageAmount = 100;
+    public GameObject Player;
+
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+
+    }
     public override void Damage(PlayerMotor player)
     {
         base.Damage(player);
         Debug.Log("Blade");
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == Player)
+        {
+            // Deal damage to the wizard
+            PlayerHealthTest playerHealth = other.GetComponent<PlayerHealthTest>();
+            if (playerHealth != null)
+            {
+                Debug.Log("colliding");
+                playerHealth.TakeDamage(damageAmount);
+            }
+            if (playerHealth = null)
+            {
+                Debug.Log("No health script");
+            }
+        }
     }
 }
